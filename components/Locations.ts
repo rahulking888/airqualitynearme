1fa8dd4b70d7fdd4dfd70f4628c919608d410490
Rahul Kumar <aadiyogietw@gmail.com>
2025-08-27 12:07:07 +0530

    sitemap

---
+++ components/Locations.ts
@@ -0,0 +1,241 @@
+export const locations = [
+  {
+  country: "united-states",
+  states: [
+    {
+      state: "alabama",
+      cities: ["huntsville","mobile","montgomery","tuscaloosa","hoover","dothan","prattville","phenix-city","vestavia-hills","opelika","alabaster","gadsden","athens","northport","daphne"],
+    },
+    {
+      state: "alaska",
+      cities: ["anchorage","fairbanks","juneau","badger","knik-fairview","college","wasilla","tanaina","kalifornsky","ketchikan","kenai","kodiak","farmers-loop"],
+    },
+    {
+      state: "arizona",
+      cities: ["phoenix","tucson","mesa","gilbert","chandler","glendale","scottsdale","peoria","tempe","surprise","goodyear","san-tan-valley","yuma","avondale","queen-creek","flagstaff","maricopa","casas-adobes","casa-grande","marana","lake-havasu-city","catalina-foothills","prescott-valley"],
+    },
+    {
+      state: "arkansas",
+      cities: ["little-rock","fort-smith","springdale","jonesboro","rogers","conway","north-little-rock","hot-springs","sherwood","paragould","cabot","van-buren","searcy","west-memphis","siloam-springs","maumelle","hot-springs-village",],
+    },
+    {
+      state: "california",
+      cities: ["los-angeles","san-diego","san-jose","san-francisco","fresno","sacramento","long-beach","oakland","bakersfield","anaheim","stockton","riverside","irvine","santa-ana","chula-vista","santa-clarita","fremont","san-bernardino","modesto","fontana","moreno-valley","oxnard","huntington-beach","glendale","elk-grove","santa-rosa","rancho-cucamonga","garden-grove","oceanside","lancaster","roseville","palmdale","corona","salinas","hayward","sunnyvale","escondido","pomona","visalia","victorville","fullerton","torrance","orange","santa-clara","clovis","simi-valley","thousand-oaks","concord","vallejo","fairfield","berkeley","antioch","menifee","east-los-angeles","carlsbad","murrieta","temecula","downey","costa-mesa","jurupa-valley","west-covina","rialto","el-monte","vacaville","burbank","el-cajon","chico","inglewood","hesperia","daly-city","tracy","vista","norwalk","san-marcos","arden-arcade","merced","manteca","indio","chino","redding","hemet","mission-viejo","carson","compton","south-gate","santa-monica","folsom","mountain-view","santa-barbara","lake-forest","citrus-heights","san-leandro","whittier","livermore","rancho-cordova","hawthorne","perris","redwood-city","newport-beach","buena-park","milpitas","upland","tustin","alameda","carmichael","chino-hills","napa","bellflower","pleasanton","rocklin","apple-valley","redlands","lake-elsinore","turlock","tulare","walnut-creek","eastvale","camarillo","baldwin-park","yuba-city","madera","lodi","redondo-beach","palo-alto","davis","yorba-linda","union-city","castro-valley","south-san-francisco","laguna-niguel","lynwood","porterville","san-clemente","florence-graham"],
+    },
+    {
+      state: "colorado",
+      cities: ["denver","colorado-springs","aurora","fort-collins","lakewood","thornton","arvada","westminster","greeley","pueblo","centennial","boulder","highlands-ranch","longmont","castle-rock","loveland","broomfield","grand-junction","commerce-city","littleton"],
+    },
+    {
+      state: "connecticut",
+      cities: ["bridgeport","stamford","new-haven","hartford","waterbury","danbury","new-britain","west-hartford","hamden","meriden","east-hartford","shelton"],
+    },
+    {
+      state: "delaware",
+      cities: ["hockessin","pike-creek-valley","claymont","seaford","north-star","wilmington-manor","millsboro","pike-creek","elsmere",],
+    },
+    {
+      state: "florida",
+      cities: ["jacksonville","miami","tampa","orlando","port-st-lucie","hialeah","cape-coral","tallahassee", "fort-lauderdale","pembroke-pines","hollywood","gainesville","miramar","palm-bay","coral-springs","west-palm-beach","lehigh-acres","lakeland","spring-hill","pompano-beach","clearwater","brandon","miami-gardens","davie","riverview","palm-coast","boca-raton","plantation","deltona","sunrise","fort-myers","north-port","alafaya","deerfield-beach","melbourne","town-n-country","daytona-beach","homestead","kissimmee","doral","miami-beach","boynton-beach","largo","pine-hills","the-villages","kendall","lauderhill","tamarac","poinciana","weston","ocala","delray-beach","port-orange"],
+    },
+    {
+      state: "georgia",
+      cities: ["atlanta","macon","savannah","athens","south-fulton","sandy-springs","roswell","warner-robins","mableton","alpharetta","marietta","stonecrest","smyrna","valdosta","dunwoody"],
+    },
+    {
+      state: "hawaii",
+      cities: ["honolulu","east-honolulu","hilo","pearl-city","kailua","waipahu","kaneohe","kahului","mililani-town","kapolei","makakilo","schofield-barracks","ocean-pointe","wailuku","kaneohe","halawa","waimalu","waianae","royal-kunia","hawaiian-paradise-park","maili","nanakuli","waihee-waiehu"],
+    },
+      {
+      state: "idaho",
+      cities: ["boise","meridian","nampa","caldwell","idaho-falls","pocatello","twin-falls","post-falls","mountain-home"],
+    },
+     {
+      state: "illinois",
+      cities: ["chicago","naperville","joliet","rockford","elgin","champaign","waukegan","cicero","schaumburg","evanston","arlington-heights","bolingbrook","decatur","palatine","skokie","des-plaines","orland-park","oak-lawn","mount-prospect","tinley-park","normal","hoffman-estates","downers-grove","buffalo-grove"],
+    },
+    {
+      state: "indiana",
+      cities: ["indianapolis", "fort-wayne", "evansville", "south-bend"],
+    },
+    {
+      state: "iowa",
+      cities: ["des-moines", "cedar-rapids", "davenport", "sioux-city"],
+    },
+    {
+      state: "kansas",
+      cities: ["wichita", "topeka", "overland-park"],
+    },
+    {
+      state: "kentucky",
+      cities: ["louisville", "lexington", "bowling-green", "owensboro"],
+    },
+    {
+      state: "louisiana",
+      cities: ["new-orleans", "baton-rouge", "shreveport", "lafayette"],
+    },
+    {
+      state: "maine",
+      cities: ["bangor"],
+    },
+    {
+      state: "maryland",
+      cities: ["baltimore", "annapolis", "frederick", "rockville"],
+    },
+    {
+      state: "massachusetts",
+      cities: ["boston", "worcester"],
+    },
+    {
+      state: "michigan",
+      cities: ["detroit", "grand-rapids", "ann-arbor", "lansing"],
+    },
+    {
+      state: "minnesota",
+      cities: ["minneapolis", "saint-paul", "duluth", ],
+    },
+    {
+      state: "mississippi",
+      cities: ["jackson", "gulfport", "biloxi", "hattiesburg"],
+    },
+    {
+      state: "missouri",
+      cities: ["kansas-city", "springfield"],
+    },
+    {
+      state: "montana",
+      cities: ["billings", "missoula", "bozeman", "helena"],
+    },
+    {
+      state: "nebraska",
+      cities: ["omaha", "lincoln", ],
+    },
+    {
+      state: "nevada",
+      cities: ["las-vegas", "reno", "henderson", "carson-city"],
+    },
+    {
+      state: "new-hampshire",
+      cities: ["nashua",],
+    },
+    {
+      state: "new-jersey",
+      cities: ["newark", "jersey-city", "trenton", "paterson"],
+    },
+    {
+      state: "new-mexico",
+      cities: ["albuquerque", "las-cruces"],
+    },
+    {
+      state: "new-york",
+      cities: ["new-york-city", "buffalo", "rochester", "albany"],
+    },
+    {
+      state: "north-carolina",
+      cities: ["charlotte", "raleigh", "durham", "greensboro"],
+    },
+    {
+      state: "north-dakota",
+      cities: ["fargo", "bismarck", "grand-forks", "minot"],
+    },
+    {
+      state: "ohio",
+      cities: ["columbus", "cleveland", "cincinnati", "toledo"],
+    },
+    {
+      state: "oklahoma",
+      cities: ["oklahoma-city", "tulsa", "norman", "stillwater"],
+    },
+    {
+      state: "oregon",
+      cities: ["portland", "eugene", "bend"],
+    },
+    {
+      state: "pennsylvania",
+      cities: ["philadelphia", "pittsburgh", "harrisburg", "allentown"],
+    },
+    {
+      state: "rhode-island",
+      cities: ["providence", "warwick", "cranston", "pawtucket"],
+    },
+    {
+      state: "south-carolina",
+      cities: ["columbia", "charleston", "myrtle-beach"],
+    },
+    {
+      state: "south-dakota",
+      cities: ["sioux-falls", "rapid-city", "pierre", "brookings"],
+    },
+    {
+      state: "tennessee",
+      cities: ["nashville", "memphis", "knoxville", "chattanooga"],
+    },
+    {
+      state: "texas",
+      cities: ["houston", "dallas", "austin", "san-antonio"],
+    },
+    {
+      state: "utah",
+      cities: ["salt-lake-city", "provo", "ogden"],
+    },
+    {
+      state: "vermont",
+      cities: ["south-burlington","brattleboro","essex-junction"]
+,
+    },
+    {
+      state: "virginia",
+      cities: ["virginia-beach", "richmond", "norfolk", "charlottesville"],
+    },
+    {
+      state: "washington",
+      cities: ["seattle", "spokane", "tacoma", "olympia"],
+    },
+    {
+      state: "west-virginia",
+      cities: ["parkersburg","weirton","beckley"]
+,
+    },
+    {
+      state: "wisconsin",
+      cities: ["milwaukee", "madison", "green-bay", "kenosha"],
+    },
+    {
+      state: "wyoming",
+      cities: ["cheyenne", "casper", "laramie", "gillette"],
+    },
+  ],
+},
+
+
+
+
+
+
+  // Canada start
+  {
+    country: "canada",
+    states: [
+      {
+        state: "ontario",
+        cities: ["toronto", "ottawa", "mississauga", "hamilton"],
+      },
+      {
+        state: "british-columbia",
+        cities: ["vancouver", "kelowna"],
+      },
+      {
+        state: "quebec",
+        cities: ["quebec-city", "gatineau"],
+      },
+      {
+        state: "alberta",
+        cities: ["calgary", "edmonton", "red-deer"],
+      },
+      {
+        state: "manitoba",
+        cities: ["winnipeg"],
+      },
+    ],
+  },
+];
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,45 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
+
+// locations.ts file se data import
+const { locations } = require("./components/Locations.ts");
+
 module.exports = {
-  siteUrl: 'https://airqualitynearme.org',
+  siteUrl: "https://airqualitynearme.org",
   generateRobotsTxt: false,
   sitemapSize: 5000,
-  additionalPaths: async (config) => {
-    const cities = [
-      'new-york', 'los-angeles', 'chicago', 'houston', 'phoenix',
-      'philadelphia', 'san-antonio', 'san-diego', 'dallas', 'san-jose',
-      'austin', 'jacksonville', 'fort-worth', 'columbus', 'charlotte',
-      'san-francisco', 'indianapolis', 'seattle', 'denver', 'washington',
-      'boston', 'el-paso', 'nashville', 'detroit', 'oklahoma-city',
-      'portland', 'las-vegas', 'memphis', 'louisville', 'baltimore',
-      'milwaukee', 'albuquerque', 'tucson', 'fresno', 'sacramento',
-      'mesa', 'kansas-city', 'atlanta', 'long-beach', 'oregon-city',
-      'colorado-springs', 'raleigh', 'miami', 'virginia-beach',
-      'omaha', 'oakland', 'minneapolis', 'tulsa', 'arlington',
-      'tampa', 'new-orleans', 'wichita', 'cleveland', 'bakersfield',
-      'aurora', 'anaheim', 'honolulu', 'santa-ana', 'riverside',
-      'corpus-christi', 'lexington', 'stockton', 'henderson',
-      'saint-paul', 'st-petersburg', 'cincinnati', 'pittsburgh',
-      'greensboro', 'anchorage', 'plano', 'lincoln', 'orlando',
-      'irvine', 'newark', 'toledo', 'durham', 'chula-vista',
-      'fort-wayne', 'jersey-city', 'st-louis', 'stockton', 'santa-clarita',
-      'norfolk', 'virginia-beach', 'reno', 'madison', 'salt-lake-city',
-      'richmond', 'boise', 'tampa', 'birmingham', 'atlanta', 'miami',
-      'des-moines', 'rochester', 'grand-rapids', 'modesto',
-      'santa-ana', 'oregon-city', 'salem', 'ann-arbor', 'providence',
-      'hartford', 'cambridge', 'new-haven', 'athens', 'wichita-falls',
-      'jackson', 'mobile', 'augusta', 'columbia-sc', 'des-oto', 'el-monte',
-      'tallahassee', 'pueblo', 'wichita', 'fayetteville', 'topeka',
-      'boise', 'chattanooga', 'borders', 'eugene', 'flint', 'hampton',
-      'irving', 'jefferson-city', 'kansas-city', 'lafayette', 'miami-gardens',
-      'naples', 'omaha', 'pensacola', 'quincy', 'rochester', 'springfield',
-      'tucson', 'utica', 'valdosta', 'wichita', 'yoakum', 'zanesville'
-    ];
-   
-    return cities.map((city) => ({
-      loc: `/current-air-quality/${city}`,
-      changefreq: 'daily',
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    }));
-  }
+
+  additionalPaths: async () => {
+    const urls = [];
+
+    // Nested structure → cities extract
+    locations.forEach((countryObj) => {
+      countryObj.states.forEach((stateObj) => {
+        stateObj.cities.forEach((city) => {
+          urls.push({
+            loc: `/current-air-quality/${city}`,
+            changefreq: "daily",
+            priority: 0.7,
+            lastmod: new Date().toISOString(),
+          });
+        });
+      });
+    });
+
+    return urls;
+  },
 };
